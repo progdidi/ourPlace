2a2951a13603fdb48ce12a5c56b36702f4269f59
Diana Sosnovskaya <progdidi@gmail.com>
2024-08-08 14:47:37 +0300

    feat: fix header, add menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-   
-});
+//hamburger
+
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
